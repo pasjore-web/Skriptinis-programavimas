b7a357091b7026dfc37df0579a2b3906ed1752c4
pasjore-web <pasjore@gmail.com>
2026-09-20 17:15:20 +0300

    Update LD1_JorunePaslaviciute.m

--- LD1/LD1_JorunePaslaviciute.m
+++ LD1/LD1_JorunePaslaviciute.m
@@ -1,4 +1,15 @@
+%
+% Paprastas skriptas
+%
 
+x = 1:32;
+y=x.^2;
+
+plot(x, y, 'o-r', x, y/3, 'xb')
+title('Dvi funkcijos')
+xlabel('X-ai')
+%%
+ylabel('F_1 [-o-]  |  F_2 [-X-]')
 
 %[appendix]{"version":"1.0"}
 %---
